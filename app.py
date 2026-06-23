b75414430ea4341bfb2c45274dd14a013de5e065
GabiLitardo <108309227+GabiLitardo@users.noreply.github.com>
2026-06-23 11:10:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
 
-# La única modificación de origen: toma el archivo desde la ruta local "transfer.txt"
 data = np.genfromtxt("transfer.txt", delimiter = "\t", skip_header = 1)
 V = data[:, 0]
 I = data[:, 1]
@@ -13,5 +12,4 @@ plt.plot(V, 1e6*I)
 plt.xlabel("V [V]")
 plt.ylabel(r"I [$\mu$A]")
 
-# Reemplazamos plt.show() por st.pyplot() para que se muestre en la web
 st.pyplot(plt.gcf())
