39eb052c1a7648e097607efb3fd7fead0eaa0aa2
GabiLitardo <108309227+GabiLitardo@users.noreply.github.com>
2026-06-23 11:14:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,15 @@ data = np.genfromtxt("transfer.txt", delimiter = "\t", skip_header = 1)
 V = data[:, 0]
 I = data[:, 1]
 
-fig = plt.figure()
+fig1 = plt.figure()
 plt.title("Transferencia")
 plt.plot(V, 1e6*I)
+plt.xlabel("V [V]")
+plt.ylabel(r"I [$\mu$A]")
+
+fig2 = plt.figure()
+plt.title("Transferencia")
+plt.plot(V, 1e6*I, color = "red")
 plt.xlabel("V [V]")
 plt.ylabel(r"I [$\mu$A]")
 
